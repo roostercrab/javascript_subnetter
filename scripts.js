eae4ba4ba902bd03b316e41e4d608b0fb07ba09f
roostercrab <jesseshea498@gmail.com>
2022-11-20 07:56:08 -0800

    script connects to html

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
 function calculateSubnet() {
-  const ip = parseInt(document.getElementById('ip').value)
-  const mask = parseInt(document.getElementById('mask').value)
+  const ip = document.getElementById('ip').value
+  const mask = document.getElementById('mask').value
   const subnet = document.getElementById('subnet')
   const broadcast = document.getElementById('broadcast')
   //console.log(x + y)
-  subnet.value = x + y
-  broadcast.value = x + y
+  subnet.value = ip
+  broadcast.value = mask
 }
